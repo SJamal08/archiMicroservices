d8ca15ab8bca10d3feef5b9742136e0a9dcb1288
SJamal08 <sidikjam08@gmail.com>
2021-07-28 14:00:05 +0200

    new archi

---
+++ microservices_market/src/main/java/com/example/microservices_market/MicroservicesMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMarketApplication.class, args);
+	}
+
+}
---
+++ microservices_membership/src/main/java/com/example/microservices_membership/MicroservicesMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices_membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMembershipApplication.class, args);
+	}
+
+}
